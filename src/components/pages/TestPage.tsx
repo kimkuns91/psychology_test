c6a8df2f7e7011987fd4c0b68966a34ee6ed342d
Kun Woo Kim <kimkuns98@gmail.com>
2024-10-01 14:46:15 +0900

    이미지 로드 속도 향상

--- src/components/pages/TestPage.tsx
+++ src/components/pages/TestPage.tsx
@@ -5,10 +5,10 @@ import {
   shuffledAImagesState,
   shuffledBImagesState,
 } from '@/atom'
+import { useEffect, useState } from 'react'
 
 import Image from 'next/image'
 import ProgressBar from '@/components/ProgressBar'
-import { useEffect } from 'react'
 import { useRecoilState } from 'recoil'
 
 const TestPage: React.FC = () => {
@@ -17,6 +17,9 @@ const TestPage: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useRecoilState(currentIndexState)
   const [currentGroup, setCurrentGroup] = useRecoilState(currentGroupState)
   const [page, setPage] = useRecoilState(pageState)
+  
+  // 이미지 로드 상태 관리
+  const [imageLoaded, setImageLoaded] = useState(false)
 
   const currentImages = currentGroup === 'A' ? shuffledAImages : shuffledBImages
   const currentImage =
@@ -32,9 +35,17 @@ const TestPage: React.FC = () => {
     setPage('question') // 질문 페이지로 전환
   }
 
+  // 이미지 로드 핸들러
+  const handleImageLoad = () => {
+    setImageLoaded(true) // 이미지 로드가 완료되면 true로 설정
+  }
+
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center relative">
-      <ProgressBar duration={10} onComplete={handleProgressComplete} />
+      {/* 이미지가 로드되면 ProgressBar 시작 */}
+      {imageLoaded && (
+        <ProgressBar duration={10} onComplete={handleProgressComplete} />
+      )}
 
       {currentImage ? (
         <Image
@@ -43,6 +54,8 @@ const TestPage: React.FC = () => {
           width={600}
           height={600}
           className="rounded-md"
+          priority
+          onLoad={handleImageLoad} // 이미지 로드 완료 핸들러
         />
       ) : (
         <h1 className="text-2xl text-red-500">이미지를 로드할 수 없습니다.</h1>
